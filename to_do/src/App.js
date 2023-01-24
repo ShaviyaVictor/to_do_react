48177c7326d852949ba3645b344aae9d3fe10a35
Victor Shaviya <vs610642@digicelgroup.com>
2023-01-24 18:00:38 -0500

    Add a Local time prop to the function

--- to_do/src/App.js
+++ to_do/src/App.js
@@ -1,6 +1,8 @@
 // import logo from './logo.svg';
 // import './App.css';
 
+import { PureComponent } from "react";
+
 // function App() {
 //   return (
 //     <div className="App">
@@ -22,20 +24,38 @@
 //   );
 // }
 
+// PureComponent
 // function App() {
 //   return (
 //     <h1>Hello World!</h1>
 //   );
 // }
 
-let App = () => {
-  return(
-    <div className="App-header">
-      <h1>
-        Hello World!
-      </h1>
+// FunctionalComponent
+// let App = () => {
+//   return(
+//     <div className="App-header">
+//       <h1>
+//         Hello World!
+//       </h1>
+//     </div>
+//   )
+// }
+
+// PureComponent with Time
+function App(props) {
+
+  const currentDate = new Date();
+
+  return (
+    <div>
+      <h1>Hello, world!</h1>
+      <h2>
+        The time right now is: <code>{currentDate.toLocaleTimeString()}</code>
+      </h2>
     </div>
   )
+
 }
 
 export default App;
